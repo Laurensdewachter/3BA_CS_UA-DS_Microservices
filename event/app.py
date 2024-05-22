8a72b01bd43460ca52da0bbbe49d0b2ed82d162f
Laurens De Wachter <93316351+Laurensdewachter@users.noreply.github.com>
2024-05-22 14:38:11 +0200

    Setup docker for event service

---
+++ event/app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from flask_restful import Api
+
+app = Flask("User")
+api = Api(app)
--- user/app.py
+++ user/app.py
@@ -75,6 +75,3 @@ class User(Resource):
 
 
 api.add_resource(User, "/user")
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port=5001)
